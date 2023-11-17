273986e6d6ee980746171e42db9e2f50f3febd8e
Tetiana-UA <izidaxm85@gmail.com>
2023-11-17 19:46:10 +0100

    6

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -19,8 +19,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-
 const list = document.querySelector('.gallery');
 
 list.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
@@ -30,24 +28,19 @@ function createMarkup(arr) {
     .map(
       ({ preview, original, description }) => `
     <li class="gallery__item">
-    <a class="gallery__link" href="${original} ">
-    <img
-    class="gallery__image"
-    src="${preview}"
-    alt="${description}"/>
-    </a>
-    </li>
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
     `
     )
     .join('');
 }
-
-//var lightbox = $('.gallery a').simpleLightbox({
-/* options */
-//});
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
 });
+
+console.log(galleryItems);
